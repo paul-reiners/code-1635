fbce43d20d91a5892347082494ac5ceec96dc70f
Paul Reiners <paul.reiners@gmail.com>
2020-03-29 14:12:56 -0500

    First version.

--- src/python/three_state_totalistic_ca.py
+++ src/python/three_state_totalistic_ca.py
@@ -3,20 +3,29 @@
 
 import numpy as np
 
+rule_width = 7
+
+
 def make_table(rule):
     """Make the table for a given CA rule.
 
-    rule: int 0-255
+    rule: int 0-2186
 
-    returns: array of 8 0s and 1s
+    returns: array of 7 0s, 1s, and 2s
     """
-    rule = np.array([rule], dtype=np.uint8)
-    table = np.unpackbits(rule)[::-1]
-    return table
+    rule_set = [0] * rule_width
+    n = rule
+    for i in range(rule_width):
+        rule_set[i] = n % 3
+        n = n // 3
+    rule_set.reverse()
+    print("number: ", rule)
+    print("rule_set:", rule_set)
+    return rule_set
 
 
-class Cell1D:
-    """Represents a 1-D a cellular automaton"""
+class TotalisticCell1D:
+    """Represents a 1-D, three-state, totalistic cellular automaton"""
 
     def __init__(self, rule, n, m=None):
         """Initializes the CA.
@@ -30,6 +39,7 @@ class Cell1D:
         array:  the numpy array that contains the data.
         next:   the index of the next empty row.
         """
+        self.rule_width = 7
         self.table = make_table(rule)
         self.n = n
         self.m = 2 * n + 1 if m is None else m
@@ -60,9 +70,64 @@ class Cell1D:
 
     def step(self):
         """Executes one time step by computing the next row of the array."""
+        # First we create an empty array for the new values
         a = self.array
         i = self.next
-        window = [4, 2, 1]
-        c = np.correlate(a[i - 1], window, mode='same')
-        a[i] = self.table[c]
+        c = self.generate(a[i-1])
+        a[i] = c
         self.next += 1
+
+    # The process of creating the new generation
+    def generate(self, cells):
+        # First we create an empty array for the new values
+        nextgen = [0] * len(cells)
+        # For every spot, determine new state by examing current state,
+        # and neighbor states
+        # Ignore edges that only have one neighor
+        for i in range(1, len(cells) - 1):
+            left = cells[i - 1]     # Left neighbor state
+            me = cells[i]           # Current state
+            right = cells[i + 1]    # Right neighbor state
+            # Compute next generation state based on ruleset
+            nextgen[i] = self.executeRules(left, me, right)
+
+        return np.asarray(nextgen)
+
+    # Implementing the Wolfram rules
+    def executeRules(self, a, b, c):
+        total = a + b + c
+        if 0 <= total <= 7:
+            return self.table[7 - total - 1]
+        else:
+            return 0
+
+
+    def print_ca(self, start=0, end=None):
+        """Prints the CA.
+
+        start: index of the first column to be shown
+        end: index of the last column to be shown
+        """
+        a = self.array[:, start:end]
+        for row in a:
+            print(row)
+
+
+def draw_ca(rule, n=32):
+    """Makes and prints a 1D, three-state, totalistic CA with a given rule.
+
+    rule: int rule number
+    n: number of rows
+    """
+    ca = TotalisticCell1D(rule, n)
+    ca.start_single()
+    ca.loop(n - 1)
+    ca.print_ca()
+
+
+def main():
+    draw_ca(rule=777, n=10)
+
+
+if __name__ == "__main__":
+    main()
