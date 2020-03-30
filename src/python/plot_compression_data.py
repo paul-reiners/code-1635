a71df438a323e3d8fc96161b72eb438e5c3b07be
Paul Reiners <paul.reiners@gmail.com>
2020-03-30 09:58:56 -0500

    Organized file names.

--- src/python/plot_compression_data.py
+++ src/python/plot_compression_data.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
-    df = pd.read_csv('/Users/reiners/Dropbox/projects/code-1635/out/1635_128_data.txt')
+    df = pd.read_csv('/Users/reiners/Dropbox/projects/code-1635/out/1635_256_data.txt')
     print(df)
     df.plot(kind='scatter', x='generation', y='compression-ratio', color='red')
     plt.show()
