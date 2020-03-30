b0370c5d93d8fda9d25d7aaac23494fe636236d9
Paul Reiners <paul.reiners@gmail.com>
2020-03-30 09:03:20 -0500

    Miscellaneous changes.

--- src/python/analyze_compression.py
+++ src/python/analyze_compression.py
@@ -1,14 +1,24 @@
+import sys
+
+from zlib import compress
+
 if __name__ == "__main__":
-    file1 = open('/Users/reiners/Dropbox/projects/code-1635/out/1635_16.txt', 'r')
+    file_path = sys.argv[1]
+    file1 = open(file_path, 'r')
     count = 0
 
     while True:
         count += 1
 
-        line = file1.readline()
+        line = file1.readline().strip()
+        orig_size = len(line)
+        if orig_size == 0:
+            continue
+        compressed_size = len(compress(line))
+        ratio = float(compressed_size) / float(orig_size)
 
         if not line:
             break
-        print("Line{}: {}".format(count, line.strip()))
+        print("{}, {}, {}".format(orig_size, compressed_size, ratio))
 
     file1.close()
