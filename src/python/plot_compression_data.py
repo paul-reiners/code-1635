7258d39e7e7be48f22819b361aa978a0d7c26d0f
Paul Reiners <paul.reiners@gmail.com>
2020-03-30 11:34:18 -0500

    Added command-line args.

--- src/python/plot_compression_data.py
+++ src/python/plot_compression_data.py
@@ -1,9 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
 
 
-if __name__ == "__main__":
-    df = pd.read_csv('/Users/reiners/Dropbox/projects/code-1635/out/1635_256_data.txt')
+def plot_data(file_path):
+    df = pd.read_csv(file_path)
     print(df)
     df.plot(kind='scatter', x='generation', y='compression-ratio', color='red')
     plt.show()
+
+
+if __name__ == "__main__":
+    rule = 1635
+    n = sys.argv[1]
+    input_file_path = "out/{}_{}_compression.txt".format(rule, n)
+    plot_data(input_file_path)
