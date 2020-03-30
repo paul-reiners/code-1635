538ef5c56367cb5dc483b6acf691edf4a3e45006
Paul Reiners <paul.reiners@gmail.com>
2020-03-30 09:20:08 -0500

    Writing compression ratio to file.

--- src/python/analyze_compression.py
+++ src/python/analyze_compression.py
@@ -2,23 +2,33 @@ import sys
 
 from zlib import compress
 
-if __name__ == "__main__":
-    file_path = sys.argv[1]
-    file1 = open(file_path, 'r')
+
+def main(in_file_path, out_file_path):
+    in_file = open(in_file_path, 'r')
+    out_file = open(out_file_path, "w")
+    out_file.write("row,width,compressed-width,compression-ratio\n")
     count = 0
 
     while True:
-        count += 1
-
-        line = file1.readline().strip()
+        line = in_file.readline().strip()
         orig_size = len(line)
         if orig_size == 0:
-            continue
+            break
         compressed_size = len(compress(line))
         ratio = float(compressed_size) / float(orig_size)
 
         if not line:
             break
-        print("{}, {}, {}".format(orig_size, compressed_size, ratio))
+        print("{}, {}, {}, {}".format(count, orig_size, compressed_size, ratio))
+        out_file.write("{},{},{},{}".format(count, orig_size, compressed_size, ratio))
+        out_file.write("\n")
+        count += 1
 
-    file1.close()
+    in_file.close()
+    out_file.close()
+
+
+if __name__ == "__main__":
+    input_file_path = sys.argv[1]
+    output_file_path = sys.argv[2]
+    main(input_file_path, output_file_path)
