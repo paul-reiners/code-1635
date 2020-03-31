a4ff60c2cb7282aac0928d9764ba9158c1d3bbd1
Paul Reiners <paul.reiners@gmail.com>
2020-03-31 09:35:51 -0500

    Changed file naming format.

--- src/python/compression/plot_data.py
+++ src/python/compression/plot_data.py
@@ -5,14 +5,14 @@ import sys
 
 def plot_data(generation_count):
     rule = 1635
-    input_file_path = "out/compression/{}_{}_compression.txt".format(rule, generation_count)
+    input_file_path = "out/compression/{}_{:05d}_compression.txt".format(rule, generation_count)
     df = pd.read_csv(input_file_path)
     print(df)
     df.plot(kind='scatter', x='generation', y='compression-ratio', color='red')
-    plt.savefig('img/{}_{}_compression.png'.format(rule, generation_count))
+    plt.savefig('img/{}_{:05d}_compression.png'.format(rule, generation_count))
     plt.show()
 
 
 if __name__ == "__main__":
-    n = sys.argv[1]
+    n = int(sys.argv[1])
     plot_data(n)
