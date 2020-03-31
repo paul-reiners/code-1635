db45b949eb1a6d0b745b6fcc107c7a5a31f504e8
Paul Reiners <paul.reiners@gmail.com>
2020-03-31 12:53:06 -0500

    Fixed a bug.

--- src/python/run_all.py
+++ src/python/run_all.py
@@ -5,12 +5,15 @@ from compression.plot_data import plot_data
 from three_state_totalistic_ca import write_ca
 
 
-def main(num_generations):
-    write_ca(num_generations)
-    create_data(num_generations)
-    plot_data(num_generations)
+def main(num_generations, seed=None):
+    write_ca(num_generations, seed)
+    create_data(num_generations, seed=seed)
+    plot_data(num_generations, seed)
 
 
 if __name__ == "__main__":
     n = int(sys.argv[1])
-    main(n)
+    if len(sys.argv) == 2:
+        main(n)
+    else:
+        main(n, sys.argv[2])
