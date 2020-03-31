62427d07bc2092a47dbe817bff90f4c2a5c63255
Paul Reiners <paul.reiners@gmail.com>
2020-03-31 11:45:37 -0500

    Can specify starting state.

--- src/python/compression/plot_data.py
+++ src/python/compression/plot_data.py
@@ -3,17 +3,28 @@ import matplotlib.pyplot as plt
 import sys
 
 
-def plot_data(generation_count):
+def plot_data(generation_count, gen0=None):
     rule = 1635
-    input_file_path = "out/compression/{}_{:05d}_compression.txt".format(rule, generation_count)
+    if gen0 is None:
+        input_file_path = "out/compression/{}_{:05d}_compression.txt".format(rule, generation_count)
+    else:
+        input_file_path = "out/compression/{}_{:05d}_{}_compression.txt".format(rule, generation_count, gen0)
     df = pd.read_csv(input_file_path)
     df = df.iloc[50:]
     print(df)
     df.plot(kind='scatter', x='generation', y='compression-ratio', color='red')
-    plt.savefig('img/{}_{:05d}_compression.png'.format(rule, generation_count))
+    if gen0 is None:
+        output_file_path = 'img/{}_{:05d}_compression.png'.format(rule, generation_count)
+    else:
+        output_file_path = 'img/{}_{:05d}_{}_compression.png'.format(rule, generation_count, gen0)
+    plt.savefig(output_file_path)
     plt.show()
 
 
 if __name__ == "__main__":
     n = int(sys.argv[1])
-    plot_data(n)
+    if len(sys.argv) == 2:
+        plot_data(n)
+    else:
+        seed = sys.argv[2]
+        plot_data(n, seed)
